1d783cf10522b67dea8ab13f07e321bbef42e11d
Luis Flores <luis.r.f.e@hotmail.com>
2023-11-24 17:03:55 -0600

    x

--- js09-NPM/assets/js/main.js
+++ js09-NPM/assets/js/main.js
@@ -1,1 +1,16 @@
-console.log('npm');
+const url = 'https://fakestoreapi.com/users';
+
+
+const getUsersUsingfetch = async (url) => {
+
+    await fetch(url)
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log(error))
+
+
+
+}
+
+
+getUsersUsingfetch(url);
